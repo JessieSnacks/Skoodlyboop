7c8c19e89a5cf69a09de14c25d84bfb5812b8561
hamadou diallo <hamadou_diallo@yahoo.com>
2024-08-15 14:02:59 -0400

    added loading for home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,24 @@
 import React, { useContext } from "react";
 import Showcase from "./Showcase";
 import { ApiContext } from "../../context/ApiCallsContext";
+import Container from "../../UI/Container";
+import classes from "./Home.module.css";
 const Home = () => {
+  const { loader } = classes;
   const { items, isLoading } = useContext(ApiContext);
-
   return (
     <>
-      {isLoading && <p>is Loading...</p>}
+      {isLoading && (
+        <section>
+          <Container>
+            <p className={loader}>
+              Welcome to the Nosh, Application is being loaded, please give it a
+              few seconds.
+              <big>Loading ...</big>
+            </p>
+          </Container>
+        </section>
+      )}
       {!isLoading && items && items.content && (
         <Showcase title={items.content.Title} />
       )}
